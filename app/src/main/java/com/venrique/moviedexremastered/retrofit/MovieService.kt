6637714649895ef9b47576d29589ef397a2a79d6
venrique <00046517@uca.edu.sv>
2019-06-08 21:51:48 -0600

    Creación de la instancia de retrofit

--- app/src/main/java/com/venrique/moviedexremastered/retrofit/MovieService.kt
+++ app/src/main/java/com/venrique/moviedexremastered/retrofit/MovieService.kt
@@ -1,7 +1,36 @@
 package com.venrique.moviedexremastered.retrofit
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.venrique.moviedexremastered.database.entidades.Movie
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 const val BASE_URL = "http://www.omdbapi.com/"
-const val TOKEN_API = "8b0b11f6"
 
 interface MovieService {
+
+    @GET("/?apikey=8b0b11f6&t={peli}")
+    fun getMovies(@Path("peli") peli: String): Deferred<Response<List<Movie>>>
+
+    companion object{
+        var INSTANCE: MovieService? = null
+
+        fun getGithubService(): MovieService{
+            if (INSTANCE != null){
+                return INSTANCE!!
+            }else{
+                INSTANCE = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                    .build()
+                    .create(MovieService::class.java)
+                return INSTANCE!!
+            }
+        }
+    }
 }
